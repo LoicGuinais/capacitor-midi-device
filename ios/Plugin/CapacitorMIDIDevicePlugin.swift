4e327716095b3f1caed0aad3ae3c60ba02b9708c
Marc Rominger <Marcrominger@gmx.de>
2023-07-10 12:06:01 +0200

    other aproach

--- ios/Plugin/CapacitorMIDIDevicePlugin.swift
+++ ios/Plugin/CapacitorMIDIDevicePlugin.swift
@@ -7,49 +7,97 @@ import Capacitor
  */
 @objc(CapacitorMIDIDevicePlugin)
 public class CapacitorMIDIDevicePlugin: CAPPlugin {
-    private let midiHandler = iOSMIDIHandler.shared
 
-    @objc func listMIDIDevices(_ call: CAPPluginCall) {
-        let devices = midiHandler.listMIDIDevices()
-        call.resolve([
-            "devices": devices
-        ])
+  @objc func listMIDIDevices(_ call: CAPPluginCall) {
+    if let midiDevices = getAvailableMIDIDevices() {
+      let deviceNames = midiDevices.map { $0.name }
+      call.resolve(["value": deviceNames])
+    } else {
+      call.reject("Failed to retrieve MIDI devices")
+    }
+  }
+
+  private func getAvailableMIDIDevices() -> [MIDIDevice]? {
+    // Implementation code for retrieving available MIDI devices
+    // This can vary depending on the platform and MIDI library you're using
+    // Here's a sample implementation using Core MIDI framework on iOS:
+
+    var devices: [MIDIDevice] = []
+    let midiClient = MIDIClientCreate("MIDIDevicePlugin" as CFString)
+
+    let destinationCount = MIDIGetNumberOfDestinations()
+    for index in 0..<destinationCount {
+      let destination = MIDIGetDestination(index)
+      if destination != 0 {
+        let device = MIDIDevice(name: getDestinationName(destination))
+        devices.append(device)
+      }
     }
 
-    @objc func openDevice(_ call: CAPPluginCall) {
-        guard let deviceNumber = call.getInt("deviceNumber") else {
-            call.reject("Missing deviceNumber parameter")
-            return
-        }
+    return devices.isEmpty ? nil : devices
+  }
 
-        midiHandler.openDevice(deviceNumber: deviceNumber) { message in
-            let msgType: String
-            switch message.type {
-            case .noteOn:
-                msgType = "NoteOn"
-            case .noteOff:
-                msgType = "NoteOff"
-            }
+  private func getDestinationName(_ destination: MIDIEndpointRef) -> String {
+    var cfName: Unmanaged<CFString>? = nil
+    let result = MIDIObjectGetStringProperty(destination, kMIDIPropertyName, &cfName)
+    if result == noErr, let cfString = cfName?.takeRetainedValue() {
+      return cfString as String
+    }
+    return ""
+  }
 
-            let msg: [String: Any] = [
-                "type": msgType,
-                "note": message.note,
-                "velocity": message.velocity
-            ]
-
-            self.notifyListeners("MIDI_MSG_EVENT", data: msg)
-        }
-
-        call.resolve()
+  @objc func openDevice(_ call: CAPPluginCall) {
+    guard let options = call.getObject("options") else {
+      call.reject("Invalid options")
+      return
     }
 
-    @objc func initConnectionListener(_ call: CAPPluginCall) {
-        midiHandler.addDeviceConnectionListener { devices in
-            self.notifyListeners("MIDI_CON_EVENT", data: [
-                "devices": devices
-            ])
-        }
-
-        call.resolve()
+    guard let deviceNumber = options["deviceNumber"] as? Int else {
+      call.reject("Invalid deviceNumber")
+      return
     }
+
+    // Implementation code for opening the MIDI device
+    // This can vary depending on the platform and MIDI library you're using
+    // Here's a sample implementation using Core MIDI framework on iOS:
+
+    let destination = MIDIGetDestination(deviceNumber)
+    if destination != 0 {
+      let deviceName = getDestinationName(destination)
+      // Open the MIDI device with the specified deviceNumber
+      // Perform any necessary setup or configuration
+
+      call.resolve()
+    } else {
+      call.reject("Failed to open MIDI device")
+    }
+  }
+
+  @objc func initConnectionListener(_ call: CAPPluginCall) {
+    // Implementation code for initializing the connection listener
+  }
+
+  @objc func addListener(_ call: CAPPluginCall) {
+    guard let eventName = call.getString("eventName") else {
+      call.error("Invalid eventName")
+      return
+    }
+
+    switch eventName {
+      case "MIDI_MSG_EVENT":
+        addMidiMessageListener(call)
+      case "MIDI_CON_EVENT":
+        addMidiConnectionListener(call)
+      default:
+        call.error("Unsupported eventName")
+    }
+  }
+
+  private func addMidiMessageListener(_ call: CAPPluginCall) {
+    // Implementation code for adding a MIDI message listener
+  }
+
+  private func addMidiConnectionListener(_ call: CAPPluginCall) {
+    // Implementation code for adding a MIDI connection listener
+  }
 }
